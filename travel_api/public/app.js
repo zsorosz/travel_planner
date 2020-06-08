4b27a4e00b51e960f0b40d355a54c1b57928fccd
Zsofia <oroszi.zsofi@gmail.com>
2020-06-08 14:25:15 +0200

    Add jQuery and write initial AJAX call

--- travel_api/public/app.js
+++ travel_api/public/app.js
@@ -1,1 +1,11 @@
-alert('Hi from app.js');
+$(document).ready(() => {
+    $.getJSON('/api/users')
+    .then(addUsers)
+});
+
+function addUsers(users) {
+    users.forEach(user => {
+        let newUser = $('<li class="userName">' + user.name + '</li>');
+        $('.list').append(newUser);   
+    });
+}
