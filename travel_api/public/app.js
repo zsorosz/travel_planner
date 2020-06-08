1e8816f70490c70611b8846dd3e88375a3c04b85
Zsofia <oroszi.zsofi@gmail.com>
2020-06-08 14:51:28 +0200

    Connect the Form to API

--- travel_api/public/app.js
+++ travel_api/public/app.js
@@ -1,11 +1,33 @@
 $(document).ready(() => {
     $.getJSON('/api/users')
     .then(addUsers)
+
+    $('#userInput').keypress((event) => {
+        if(event.which == 13) {
+            createUser()
+        }
+    });
 });
 
 function addUsers(users) {
     users.forEach(user => {
-        let newUser = $('<li class="userName">' + user.name + '</li>');
-        $('.list').append(newUser);   
+        addUser(user)  
     });
+};
+
+function addUser(user) {
+    let newUser = $('<li class="userName">' + user.name + '</li>');
+    $('.list').append(newUser);   
+}
+
+function createUser() {
+    let input = $('#userInput').val();
+    $.post('/api/users', {name: input})
+    .then((newUser) => {
+        $('#userInput').val('');
+        addUser(newUser)
+    })
+    .catch((err) => {
+        console.log(err);      
+    })
 }
