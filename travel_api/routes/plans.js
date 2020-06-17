e3d2bc74e6fece0538c2a37591e8e0c3cc079240
Zsofia <oroszi.zsofi@gmail.com>
2020-06-17 13:41:40 +0200

    Add create new plan route

--- travel_api/routes/plans.js
+++ travel_api/routes/plans.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const db = require('../models');
 const User = require('../models/user');
 const Plans = require('../models/plans');
+
+//Show all Plans
 
 router.get("/", function(req, res){
     User.findById(req.params.id).populate("plans").exec(function(err, user){
@@ -21,13 +23,45 @@ router.get("/", function(req, res){
     });
 });
 
+//Create new plan
 
-router.get('/', function(req, res){
-    db.User.findById(req.params.userId).then((foundUser) => {
-        res.json(foundUser);
-    }).catch((err) => {
-        res.send(err)
-    })
-})
+router.post("/new", function(req, res){
+    User.findById(req.params.id).populate("plans").exec(function(err, user){
+        if(err){
+            res.redirect("back");
+        } else{
+            const newPlan = {
+                startDate: req.body.startDate,
+                endDate: req.body.endDate,
+                route: {
+                    departureCity: req.body.departureCity,
+                    arrivalCity: req.body.arrivalCity,
+                    departureDate: req.body.departureDate,
+                    arrivalDate: req.body.arrivalDate,
+                    travelMethod: req.body.travelMethod
+                }
+            }
+            Plans.create(newPlan, function(err, plan){
+                if(err){
+                    console.log(err);
+                } else {   
+                    // user.save();
+                    plan.save();
+                };
+                user.plans.push(plan);
+                user.save();
+                res.json(plan);
+            });
+        }
+    });
+});
+
+// router.get('/', function(req, res){
+//     db.User.findById(req.params.userId).then((foundUser) => {
+//         res.json(foundUser);
+//     }).catch((err) => {
+//         res.send(err)
+//     })
+// })
 
 module.exports = router;
