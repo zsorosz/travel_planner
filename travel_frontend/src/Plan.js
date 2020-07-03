27c229ca88aeb154022b64ccf6da2a61a7727615
Zsofia <oroszi.zsofi@gmail.com>
2020-07-03 10:54:50 +0200

    Adjust Plan UI

--- travel_frontend/src/Plan.js
+++ travel_frontend/src/Plan.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
 import { showPlan, deletePlan } from './PlanFunctions';
 import './styles/Plan.css';
 
@@ -57,10 +58,12 @@ class Plan extends Component{
         if(this.state.loading){
             return (<h3>Loading...</h3>)
         }
+        const { title } = this.state.plan;
+        const { departureCity, arrivalCity, departureDate, arrivalDate, travelMethod, travelCosts, accomodationCosts, otherCosts } = this.state.plan.route;
         return(
             <Container component="main" maxWidth="xl" className='plan-container'>
                 <Typography variant="h4" component="h4">
-                    {this.state.plan.title}
+                    {title}
                 </Typography>
                 <div className='plan-cta'>
                     <Link to='/profil' onClick={e => e.stopPropagation()} className='link'>
@@ -90,13 +93,14 @@ class Plan extends Component{
                 <Card className='plan-card'>
                     <CardContent>
                         <Typography variant="h5" component="h2" gutterBottom>
-                            From: {this.state.plan.route.departureCity} ------ To: {this.state.plan.route.arrivalCity}
+                            From: {departureCity} ------ To: {arrivalCity}
                         </Typography>
+                        {departureDate &&
                         <Typography color="textSecondary" gutterBottom>
-                            Date: {this.formatDate(this.state.plan.route.departureDate)}  -  {this.formatDate(this.state.plan.route.arrivalDate)}
-                        </Typography>
+                            Date: {this.formatDate(departureDate)}  -  {this.formatDate(arrivalDate)}
+                        </Typography>}
                         <Typography color="textSecondary" gutterBottom>
-                            Travel by: {this.state.plan.route.travelMethod}
+                            Travel by: {travelMethod}
                         </Typography>
                     </CardContent>
                     <CardActions>     
@@ -114,18 +118,23 @@ class Plan extends Component{
                         <Typography variant="h5" component="h2" gutterBottom>
                             Travel Costs
                         </Typography>
+                        {travelCosts &&
                         <Typography color="textSecondary" gutterBottom>
-                            Transportation: {this.state.plan.route.travelCosts} €
-                        </Typography>
+                            Transportation: {travelCosts} €
+                        </Typography>}
+                        {accomodationCosts &&
                         <Typography color="textSecondary" gutterBottom>
-                            Accomodation: {this.state.plan.route.accomodationCosts} €
-                        </Typography>
+                            Accomodation: {accomodationCosts} €
+                        </Typography>}
+                        {otherCosts &&
                         <Typography color="textSecondary" gutterBottom>
-                            Other Costs: {this.state.plan.route.otherCosts} €
-                        </Typography>
-                        <Typography color="textSecondary" gutterBottom>
-                            Total Costs: {this.state.plan.route.otherCosts + this.state.plan.route.travelCosts + this.state.plan.route.accomodationCosts} €
-                        </Typography>
+                            Other Costs: {otherCosts} €
+                        </Typography>}
+                        <hr className='divider' />
+                        {(travelCosts || accomodationCosts || otherCosts) &&
+                        <Typography variant="h6" gutterBottom>
+                            Total Costs: {otherCosts + travelCosts + accomodationCosts} €
+                        </Typography>}
                     </CardContent>
                     <CardActions>     
                         <Link to={{ pathname: `/${this.state.userId}/plans/${this.state.planId}/editCosts`, plan: this.state.plan }} onClick={e => e.stopPropagation()} className='link'>
